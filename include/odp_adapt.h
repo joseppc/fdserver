8ad983019e690710ea3a052e66290768cd6aaeeb
Josep Puigdemont <josep.puigdemont@linaro.org>
2018-04-27 09:33:50 +0200

    Add missing license and (c) headers
    Signed-off-by: Josep Puigdemont <josep.puigdemont@linaro.org>

--- include/odp_adapt.h
+++ include/odp_adapt.h
@@ -1,3 +1,8 @@
+/* Copyright (c) 2018, Linaro Limited
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
 #ifndef ODP_ADAPT_H
 #define ODP_ADAPT_H
 
--- src/include/fdserver_internal.h
+++ src/include/fdserver_internal.h
@@ -1,3 +1,8 @@
+/* Copyright (c) 2018, Linaro Limited
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
 #ifndef FDSERVER_INTERNAL
 #define FDSERVER_INTERNAL
 
